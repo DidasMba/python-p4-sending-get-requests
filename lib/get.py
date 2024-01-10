e7b6ed358205c3b85904e6e80ca04920e4fd26aa
DidasMba <daniel.mbarushi@gmail.com>
2024-01-10 21:49:18 +0300

    importrequests

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,5 @@
 import requests
-import json
-
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
-
+url = "https://learn-co-curriculum.github.io/json-site-example/"
 response = requests.get(url)
+print(response.text)
 
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
